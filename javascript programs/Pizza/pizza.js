a420953d1838dab0198e8de9c421a797e00c9d6f
Rajendra Babu <kappala.rajendra-babu@capgemini.com>
2018-12-07 18:38:23 +0530

    Java Script Assignments

--- javascript programs/Pizza/pizza.js
+++ javascript programs/Pizza/pizza.js
@@ -4,13 +4,14 @@ function isNumber(x) {
     if (x.value.length == 0) {
         alert("message");
         return false;
-    } else {
+    } 
+    else {
         alert("true");
         return true;
     }
 }
 function IsEmpty() {
-    if (document.forms['frm'].question.value === "") {
+    if ( document.forms['frm']['name'].value === "") {
         alert("empty");
         return false;
     }
@@ -31,15 +32,57 @@ function tbl() {
     rate_value = '';
     if (document.getElementById('delivery').checked) {
         delivery = true;
-    } else if (document.getElementById('pickup').checked) {
+    } 
+    else if (document.getElementById('pickup').checked) {
         delivery = false;
     }
+    var Toppings =[];
+    if (document.getElementById("ExtraCheese").checked) {
+        Toppings .push("Extra Cheese");
+    }
+    if (document.getElementById("Pepperoni").checked) {
+        Toppings .push("Pepperoni");
+    }
+    if (document.getElementById("Olives").checked) {
+        Toppings .push("Olives");
+    }
+    if (document.getElementById("Pepper").checked) {
+        Toppings .push("Pepper");
+    }
+    if (document.getElementById("Bacon").checked) {
+        Toppings .push("Bacon");
+    }
+    if (document.getElementById("Tomatoes").checked) {
+        Toppings .push("Tomatoes");
+    }
     if (document.getElementById("Mushrooms").checked) {
-        delivery = false;
+        Toppings .push("Mushrooms");
+    }
+    console.log(Toppings);
+    for(i=0;i<Toppings.length;i++){
+        insertRow(i,Toppings[i]);
     }
     document.getElementById("tName").innerHTML = document.getElementById("name").value;
     document.getElementById("tEmail").innerHTML = document.getElementById("email").value;
     document.getElementById("tAddress").innerHTML = document.getElementById("AddressText").value;
+    document.getElementById("tDelivery").innerHTML = delivery;
+    document.getElementById("tTip").innerHTML = document.getElementById("tip").value+" %";
+    var basePrice=10;
+    var deliveryCharge=0;
+    if(delivery){
+        deliveryCharge=5;
+    }
+    var tipPercent=document.getElementById("tip").value;
+    var total=(basePrice + (1.5*Toppings.length) + deliveryCharge)*(1.0 + tipPercent/100);
+    document.getElementById("tTotal").innerHTML =total;
+}
+function insertRow(i,Toppings){
+    var table = document.getElementById("Summary");
+    var nameRow = table.insertRow(i+4);
+    var nameCell1 = nameRow.insertCell(0);
+    nameCell1.innerHTML = "Toppings "+ ++i;
+    var nameCell2 = nameRow.insertCell(1);
+    nameCell2.innerHTML = Toppings;
 }
 function createTable() {
     var table = document.getElementById("Summary");
@@ -47,8 +90,19 @@ function createTable() {
     var nameCell1 = nameRow.insertCell(0);
     nameCell1.innerHTML = "Name";
     var nameCell2 = nameRow.insertCell(1);
-    nameCell2.innerHTML = "Raj";
+    nameCell2.innerHTML = "Shubham";
     var emailRow = table.insertRow(1);
     var nameCell1 = nameRow.insertCell(0);
     nameCell1.innerHTML = "Name";
 }
+function checkform()
+    {
+        var f = document.forms["frm"].elements;
+        var cansubmit = true;
+        for (var i = 0; i < f.length; i++) {
+            if (f[i].value.length == 0) cansubmit = false;
+        }
+        if (cansubmit) {
+            document.getElementById('submitbutton').disabled = !cansubmit;
+        }
+    }
